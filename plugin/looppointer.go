8dcc441656fe0b63f9d7abdcb274464d3c7992e6
Kyoichiro Yamada <me@kyoh86.dev>
2020-06-28 12:35:47 +0900

    add plugin

---
+++ plugin/looppointer.go
@@ -0,0 +1,21 @@
+// This must be package main
+// See: https://golangci-lint.run/contributing/new-linters/#how-to-add-a-private-linter-to-golangci-lint
+
+package main
+
+import (
+	linters "github.com/kyoh86/looppointer"
+	"golang.org/x/tools/go/analysis"
+)
+
+type analyzerPlugin struct{}
+
+// This must be implemented
+func (*analyzerPlugin) GetAnalyzers() []*analysis.Analyzer {
+	return []*analysis.Analyzer{
+		linters.Analyzer,
+	}
+}
+
+// This must be defined and named 'AnalyzerPlugin'
+var AnalyzerPlugin analyzerPlugin //nolint:unused
